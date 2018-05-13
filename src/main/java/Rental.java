0ab9380ef6ec9adcbce82b9236ca12f52102bb5b
Arne Artur Schulze <arne.artur.schulze@sap.com>
2018-05-13 13:23:51 +0200

    Move getAmount and getFrequentRenterPoints to Movie

--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -12,29 +12,9 @@ class Rental {
         return movie;
     }
     public double getAmount() {
-        double thisAmount = 0;
-        switch (movie.getPriceCode()) {
-            case REGULAR:
-                thisAmount += 2;
-                if (daysRented > 2)
-                    thisAmount += (daysRented - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                thisAmount += daysRented * 3;
-                break;
-            case CHILDRENS:
-                thisAmount += 1.5;
-                if (daysRented > 3)
-                    thisAmount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
+        return movie.getAmount(daysRented);
     }
     public int getFrequentRenterPoints() {
-        if(movie.getPriceCode() == PriceCode.NEW_RELEASE && daysRented > 1) {
-            return 2;
-        }
-
-        return 1;
+        return movie.getFrequentRenterPoints(daysRented);
     }
 }
