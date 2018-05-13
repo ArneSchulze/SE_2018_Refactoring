4c004b6e05c6d72ac573b61efb386f681d4ccd56
Arne Artur Schulze <arne.artur.schulze@sap.com>
2018-05-13 13:43:30 +0200

    Replace for with while loops + Use StringBuilder instead of String concatenation

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -4,46 +4,50 @@ import java.util.*;
 
 class Customer {
     private String name;
-    private Vector rentals = new Vector();
+    private ArrayList<Rental> rentals = new ArrayList<Rental>();
     public Customer (String newname){
         name = newname;
     };
-    public void addRental(Rental arg) {
-        rentals.addElement(arg);
+    public void addRental(Rental rental) {
+        rentals.add(rental);
     };
     public String getName (){
         return name;
     };
     public String statement() {
-        Enumeration enum_rentals = rentals.elements();	    
-        String result = "Rental Record for " + this.getName() + "\n";
-        result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
+        StringBuilder result = new StringBuilder("Rental Record for " + this.getName() + "\n");
+        result.append("\tTitle\t\tDays\tAmount\n");
 
-        while (enum_rentals.hasMoreElements()) {
-            Rental each = (Rental) enum_rentals.nextElement();
-            //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getAmount()) + "\n";
+        for (Rental rental: rentals) {
+            result
+                    .append("\t")
+                    .append(rental.getMovie().getTitle())
+                    .append("\t\t")
+                    .append(rental.getDaysRented())
+                    .append("\t")
+                    .append(String.valueOf(rental.getAmount()))
+                    .append("\n");
         }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
-        return result;
+        result
+                .append("Amount owed is ")
+                .append(String.valueOf(getTotalCharge()))
+                .append("\nYou earned ")
+                .append(String.valueOf(getTotalFrequentRenterPoints()))
+                .append(" frequent renter points");
+        return result.toString();
     }
     public double getTotalCharge() {
         double result = 0;
-        Enumeration rentals = this.rentals.elements();
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-            result += each.getAmount();
+        for (Rental rental: rentals) {
+            result += rental.getAmount();
         }
         return result;
     }
     public int getTotalFrequentRenterPoints() {
         int result = 0;
-        Enumeration rentals = this.rentals.elements();
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-            result += each.getFrequentRenterPoints();
+        for (Rental rental: rentals) {
+            result += rental.getFrequentRenterPoints();
         }
         return result;
     }
