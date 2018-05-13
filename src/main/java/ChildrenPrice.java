e9a6fa19807a28b315ad25a0bed233c1a96a58cb
Arne Artur Schulze <arne.artur.schulze@sap.com>
2018-05-13 13:53:51 +0200

    Remove spelling errors

--- src/main/java/ChildrenPrice.java
+++ src/main/java/ChildrenPrice.java
@@ -1,4 +1,4 @@
-public class ChildrensPrice implements Price {
+public class ChildrenPrice implements Price {
     public double getAmount(int daysRented) {
         if(daysRented > 3) {
             return (daysRented - 2) * 1.5;
